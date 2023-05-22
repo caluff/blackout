ab9da80adda63c0ae0672a6f6ed85b45c5f905f8
Caluff <dcaluff@gmail.com>
2023-05-22 17:26:28 -0400

    arreglando temaño del fetch

--- src/app/offer/page.js
+++ src/app/offer/page.js
@@ -5,32 +5,30 @@ import Loading from "@/app/offer/loading";
 
 async function Offer() {
     return (
-        <>
-            <div className={"flex flex-col text-slate-200"}>
-                {[
-                    ['cocteles_de_la_casa', 'Cocteles de la Casa'],
-                    ['especialidades_de_cafe', 'Especialidades de Café'],
-                    ['cocteles', 'Cocteles'],
-                    ['bebidas', 'Bebidas'],
-                    ['dulces', 'Dulces'],
-                    ['elaborados', 'Elaborados']
-                ].map(([id, title]) => (
-                    <div key={id} className={"relative z-20"}>
-                        <div className={"flex items-center"}>
-                            <h1 className={"text-2xl sm:text-3xl font-extrabold tracking-tight text-indigo-400 my-4"}>
-                                {title}
-                            </h1>
-                        </div>
-                        <Suspense fallback={<Loading/>}>
-                            <div className={"grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"}>
-                                <FetchPost idTitle={id}/>
-                            </div>
-                        </Suspense>
+        <div className={"flex flex-col text-slate-200"}>
+            {[
+                ['cocteles_de_la_casa', 'Cocteles de la Casa'],
+                ['especialidades_de_cafe', 'Especialidades de Café'],
+                ['cocteles', 'Cocteles'],
+                ['bebidas', 'Bebidas'],
+                ['dulces', 'Dulces'],
+                ['elaborados', 'Elaborados']
+            ].map(([id, title]) => (
+                <div key={id} className={"relative z-20 border-b border-slate-50/10"}>
+                    <div className={"flex items-center"}>
+                        <h1 className={"text-2xl sm:text-3xl font-extrabold tracking-tight text-indigo-400 my-4"}>
+                            {title}
+                        </h1>
                     </div>
-                ))}
-            </div>
+                    <Suspense fallback={<Loading/>}>
+                        <div className={"grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pb-4"}>
+                            <FetchPost idTitle={id}/>
 
-        </>
+                        </div>
+                    </Suspense>
+                </div>
+            ))}
+        </div>
     )
 }
 
