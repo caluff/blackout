3952d13c3aaa51625f2e42876d128c96da1c0407
Caluff <dcaluff@gmail.com>
2023-06-02 00:12:04 -0400

    terminado el componente Breadcrumb.jsx cambiar breadcrumbs al enfocar section

--- src/app/offer/page.js
+++ src/app/offer/page.js
@@ -1,17 +1,11 @@
 import FetchPost from "@/components/FetchPost";
 import {Suspense} from "react";
 import Loading from "@/app/offer/loading";
+import {categoriesFetch} from "@/constants";
 async function Offer() {
     return (
         <div className={"flex flex-col text-slate-200"}>
-            {[
-                ['cocteles_de_la_casa', 'Cocteles de la Casa'],
-                ['especialidades_de_cafe', 'Especialidades de Café'],
-                ['cocteles', 'Cocteles'],
-                ['bebidas', 'Bebidas'],
-                ['dulces', 'Dulces'],
-                ['elaborados', 'Elaborados']
-            ].map(([id, title]) => (
+            {categoriesFetch.map(([id, title]) => (
                 <section id={id} key={id} className={"relative z-20 border-b border-slate-50/10"}>
                     <div className={"flex items-center"}>
                         <h1 className={"text-2xl sm:text-3xl font-extrabold tracking-tight text-indigo-400 my-4"}>
